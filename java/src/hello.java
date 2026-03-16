1c2cbb3dca103e13e07f7040cafc110fcce29c00
13042 <2562264788@qq.com>
2026-03-16 20:00:00 +0800

    Changes

---
+++ java/src/hello.java
@@ -0,0 +1,31 @@
+public class hello {
+    //第一题
+    public static void main(String[] args) {
+        System.out.println("善学如春起之苗");
+        System.out.println("不见其增，日有所长");
+        System.out.println("假学如磨刀之石");
+        System.out.println("不见其损，年有所亏");
+        System.out.println("加油吧！少年");
+        System.out.println("J");
+        System.out.println("A");
+        System.out.println("V");
+        System.out.println("A");
+        System.out.println("!");
+        //第二题
+        System.out.println(
+                "-2147483648" +
+                "\n-100" +
+                "\n0" +
+                "\n100" +
+                "\n2147483647" +
+                "\n-100" +
+                "\n-10" +
+                "\n0.0" +
+                "\n10.9" +
+                "\n100.9");
+        //第三题
+        System.out.println("java是一门跨平台的计算机语言\n" +
+                "被称为一次编写，处处运行");
+
+    }
+}
